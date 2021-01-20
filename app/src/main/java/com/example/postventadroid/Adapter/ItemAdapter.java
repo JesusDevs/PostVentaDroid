34d8f46d9025cfbadd6f3dba36329bba715a0eab
Leon RO <leon.rodriguez.ore@gmail.com>
2021-01-20 15:29:46 -0300

    falta agregar Fragmentos en la segunda actividad
    agregar fragmentos
    
    libreria glide ok
    creación de lista de datos ok
    
    pasar de actividad ok

--- app/src/main/java/com/example/postventadroid/Adapter/ItemAdapter.java
+++ app/src/main/java/com/example/postventadroid/Adapter/ItemAdapter.java
@@ -1,41 +1,78 @@
 package com.example.postventadroid.Adapter;
 
-import android.view.View;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.example.postventadroid.databinding.ItemLayoutBinding;
+
+import java.util.List;
+
 //Extender desde la clase interna generada ApartamentViewHolder ,
 
 public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ApartamentViewHolder> {
+    //representación de lo datos tipo items
+    private List<Item> itemList;
+
+    public ItemAdapter(Object apartment) {
+    }
+
+    //generar constructor posterior para instanciar el adaptador , ultimo paso
+    public ItemAdapter(List<Item> itemList) {
+        this.itemList = itemList;
+    }
+
     @NonNull
     @Override
     public ApartamentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        //instanciar biding inflar vista de recycler view , se da contexto con el viewGroup parent
+        ItemLayoutBinding binding = ItemLayoutBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+        return new ApartamentViewHolder(binding);
     }
 
-    @Override
+
+    //une las vistas por su posición ,objeto u item view holder de mi clase autogenerada ApartamentViewHolder, se encuentra por posición
     public void onBindViewHolder(@NonNull ApartamentViewHolder holder, int position) {
 
+        //crear la variable itemlistHolder donde quedara guardado el objeto según su posición del itemList
+        Item itemListHolder = itemList.get(position);
+
+        //llamar el getters de la clase creada item mediante metodo getitemdescription
+        holder.textView.setText(itemListHolder.getItemDescription());
+        //mostrar imagen de internet ,holder es el obejto donde se encuentra la imagen
+        Glide.with(holder.imageView.getContext()).load(itemListHolder.getUrlImage()).into(holder.imageView);
+
     }
 
 
     @Override
 
-    @NonNull
+
     public int getItemCount() {
-        return 0;
+        return itemList.size();
     }
 
     //clase interna  generada  puede ser una clase tipo cantante,perro gato ,
     //con atributos en xml XXX ViewHolder , contenedor de las vistas
-    public class ApartamentViewHolder extends RecyclerView.ViewHolder{
+    public class ApartamentViewHolder extends RecyclerView.ViewHolder {
+        private ImageView imageView;
+        private TextView textView;
 
-        public ApartamentViewHolder(@NonNull View itemView) {
-            super(itemView);
-
+        //modificar el View por el metodo Binding en este caso "ItemLayoutBinding"
+        //nombre variable modificado y en la super devolverle una vista con metodo binding
+        public ApartamentViewHolder(@NonNull ItemLayoutBinding itemBinding) {
+            super(itemBinding.getRoot());
+            textView = itemBinding.TextMultiLine;
+            imageView = itemBinding.imageView;
 
         }
     }
+
+
+
 }
--- app/src/main/java/com/example/postventadroid/FirstFragment.java
+++ app/src/main/java/com/example/postventadroid/FirstFragment.java
@@ -9,6 +9,11 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.example.postventadroid.Adapter.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FirstFragment extends Fragment {
 
     @Override
@@ -32,4 +37,39 @@ public class FirstFragment extends Fragment {
         });
     }
 
+    //generar listado  de items
+    public static List<Item> apartmentList() {
+        List<Item> apartmentList = new ArrayList<>();
+        Item apartment01 = new Item("Edificio Almendro", "Torre 1", "depto 2002", "san jose 345, La florida , Santiago", "https://unsplash.com/photos/Ub9LkIWxyec/download?force=true&w=640");
+        Item apartment02 = new Item("Edificio Almendro", "Torre 1", "depto 2003", "san jose 345, La florida, Santiago", "https://unsplash.com/photos/Ub9LkIWxyec/download?force=true&w=640");
+        Item apartment03 = new Item("Edificio Almendro", "Torre 1", "depto 1601", "san jose 345, La florida, Santiago", "https://unsplash.com/photos/Ub9LkIWxyec/download?force=true&w=640");
+        Item apartment04 = new Item("Edificio Alerces", "Torre b", "depto 1602", "nueva providencia 1801, Providencia", "https://unsplash.com/photos/PhYq704ffdA/download?force=true&w=640");
+        Item apartment05 = new Item("Edificio Alerces", "Torre b", "depto 1801", "nueva providencia 1801, Providencia", "https://unsplash.com/photos/PhYq704ffdA/download?force=true&w=640");
+        Item apartment06 = new Item("Edificio Legado 2", "Torre a", "depto 801", "Alcalde pedro alarcon 925, San Miguel", "https://unsplash.com/photos/jU9VAZDGMzs/download?force=true&w=640");
+        Item apartment07 = new Item("Edificio Legado 2", "Torre a", "depto 902", "Alcalde pedro alarcon 925, San Miguel", "https://unsplash.com/photos/jU9VAZDGMzs/download?force=true&w=640");
+        Item apartment08 = new Item("Edificio Legado 2", "Torre a", "depto 909", "Alcalde pedro alarcon 925, San Miguel", "https://unsplash.com/photos/jU9VAZDGMzs/download?force=true&w=640");
+        Item apartment09 = new Item("Edificio Legado 2", "Torre a", "depto 1901", "Alcalde pedro alarcon 925, San Miguel", "https://unsplash.com/photos/jU9VAZDGMzs/download?force=true&w=640");
+        Item apartment10 = new Item("Condominio nuevo mirador", "Torre 1", "depto 102", "Costanera norte 245, Vitacura", "https://unsplash.com/photos/pPxhM0CRzl4/download?force=true&w=640");
+        Item apartment11 = new Item("Condominio nuevo mirador", "Torre 1", "depto 103", "Costanera norte 245, Vitacura", "https://unsplash.com/photos/pPxhM0CRzl4/download?force=true&w=640");
+        Item apartment12 = new Item("Condominio nuevo mirador", "Torre 1", "depto 1501", "Costanera norte 245, Vitacura", "https://unsplash.com/photos/pPxhM0CRzl4/download?force=true&w=640");
+        Item apartment13 = new Item("Condominio nuevo mirador", "Torre 1", "depto 1503", "Costanera norte 245, Vitacura", "https://unsplash.com/photos/pPxhM0CRzl4/download?force=true&w=640");
+        Item apartment14 = new Item("Condominio nuevo mirador", "Torre 2", "depto 2001", "Costanera norte 245, Vitacura", "https://unsplash.com/photos/pPxhM0CRzl4/download?force=true&w=640");
+        Item apartment15 = new Item("Condominio nuevo mirador", "Torre 2", "depto 2201", "Costanera norte 245, Vitacura", "https://unsplash.com/photos/pPxhM0CRzl4/download?force=true&w=640");
+        apartmentList.add(apartment01);
+        apartmentList.add(apartment02);
+        apartmentList.add(apartment03);
+        apartmentList.add(apartment04);
+        apartmentList.add(apartment05);
+        apartmentList.add(apartment06);
+        apartmentList.add(apartment07);
+        apartmentList.add(apartment08);
+        apartmentList.add(apartment09);
+        apartmentList.add(apartment10);
+        apartmentList.add(apartment11);
+        apartmentList.add(apartment12);
+        apartmentList.add(apartment13);
+        apartmentList.add(apartment14);
+        apartmentList.add(apartment15);
+        return apartmentList;
+    }
 }
