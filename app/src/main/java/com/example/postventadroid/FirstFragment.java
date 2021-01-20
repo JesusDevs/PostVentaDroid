01bcaa8d93e5d383067e9930e2fb352b81cd0ec4
Leon RO <leon.rodriguez.ore@gmail.com>
2021-01-20 00:26:40 -0300

    APK de Post Venta inmobiliarias
    -creación de layout item data
    -adpatador
    -implemnto Recycler view
    -libreria de glide
    -listado de item
    -crear clase item

--- app/src/main/java/com/example/postventadroid/FirstFragment.java
+++ app/src/main/java/com/example/postventadroid/FirstFragment.java
@@ -1,5 +1,6 @@
 package com.example.postventadroid;
 
+import android.content.ClipData;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,8 +9,20 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.postventadroid.Adapter.ItemAdapter;
+import com.example.postventadroid.Model.Item;
+import com.example.postventadroid.databinding.FragmentFirstBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FirstFragment extends Fragment {
+    //declarar bidindg
+    private FragmentFirstBinding binding;
+
+
 
     @Override
     public View onCreateView(
@@ -17,11 +30,20 @@ public class FirstFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        binding = FragmentFirstBinding.inflate(inflater,container,false);
+        return binding.getRoot();
     }
-
+    //forma reoomendada para trabajar con las vistas xml en este caso nuestro rycler view
+    // el cual contiene un adaptery debe ser instanciado
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+//instancia itemAdapter= se ingresa el metodo apartment list el cual contiene el listado de objetos en un array List
+        ItemAdapter itemAdapter = new ItemAdapter(apartmentList());
+        binding.rvItem.setAdapter(itemAdapter);
+        //administrador de Layouts , existen 3 tipos de administradores , linear layout manager
+        binding.rvItem.setLayoutManager(new LinearLayoutManager(getContext()));
+
 
         view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,5 +53,42 @@ public class FirstFragment extends Fragment {
             }
         });
     }
+    //generar listado  de items
+    public static List<Item> apartmentList() {
+        List<Item> apartmentList = new ArrayList<>();
+        Item apartment01 = new Item ("Edificio Almendro","Torre 1", "depto 2002", "san jose 345, La florida , Santiago", "https://unsplash.com/photos/Ub9LkIWxyec/download?force=true&w=640");
+        Item apartment02 = new Item("Edificio Almendro","Torre 1", "depto 2003", "san jose 345, La florida, Santiago", "https://unsplash.com/photos/Ub9LkIWxyec/download?force=true&w=640");
+        Item apartment03 = new Item("Edificio Almendro","Torre 1", "depto 1601", "san jose 345, La florida, Santiago", "https://unsplash.com/photos/Ub9LkIWxyec/download?force=true&w=640");
+        Item apartment04 = new Item("Edificio Alerces","Torre b", "depto 1602", "nueva providencia 1801, Providencia", "https://unsplash.com/photos/PhYq704ffdA/download?force=true&w=640");
+        Item apartment05 = new Item("Edificio Alerces","Torre b", "depto 1801", "nueva providencia 1801, Providencia", "https://unsplash.com/photos/PhYq704ffdA/download?force=true&w=640");
+        Item apartment06 = new Item("Edificio Legado 2","Torre a", "depto 801", "Alcalde pedro alarcon 925, San Miguel", "https://unsplash.com/photos/jU9VAZDGMzs/download?force=true&w=640");
+        Item apartment07 = new Item("Edificio Legado 2","Torre a", "depto 902", "Alcalde pedro alarcon 925, San Miguel", "https://unsplash.com/photos/jU9VAZDGMzs/download?force=true&w=640");
+        Item apartment08 = new Item("Edificio Legado 2","Torre a", "depto 909", "Alcalde pedro alarcon 925, San Miguel", "https://unsplash.com/photos/jU9VAZDGMzs/download?force=true&w=640");
+        Item apartment09 = new Item("Edificio Legado 2","Torre a", "depto 1901", "Alcalde pedro alarcon 925, San Miguel", "https://unsplash.com/photos/jU9VAZDGMzs/download?force=true&w=640");
+        Item apartment10 = new Item("Condominio nuevo mirador","Torre 1", "depto 102", "Costanera norte 245, Vitacura", "https://unsplash.com/photos/pPxhM0CRzl4/download?force=true&w=640");
+        Item apartment11 = new Item("Condominio nuevo mirador","Torre 1", "depto 103", "Costanera norte 245, Vitacura", "https://unsplash.com/photos/pPxhM0CRzl4/download?force=true&w=640");
+        Item apartment12 = new Item("Condominio nuevo mirador","Torre 1", "depto 1501", "Costanera norte 245, Vitacura", "https://unsplash.com/photos/pPxhM0CRzl4/download?force=true&w=640");
+        Item apartment13 = new Item("Condominio nuevo mirador","Torre 1", "depto 1503", "Costanera norte 245, Vitacura", "https://unsplash.com/photos/pPxhM0CRzl4/download?force=true&w=640");
+        Item apartment14 = new Item ("Condominio nuevo mirador","Torre 2", "depto 2001", "Costanera norte 245, Vitacura", "https://unsplash.com/photos/pPxhM0CRzl4/download?force=true&w=640");
+        Item apartment15 = new Item ("Condominio nuevo mirador","Torre 2", "depto 2201", "Costanera norte 245, Vitacura", "https://unsplash.com/photos/pPxhM0CRzl4/download?force=true&w=640");
+        apartmentList.add(apartment01);
+        apartmentList.add(apartment02);
+        apartmentList.add(apartment03);
+        apartmentList.add(apartment04);
+        apartmentList.add(apartment05);
+        apartmentList.add(apartment06);
+        apartmentList.add(apartment07);
+        apartmentList.add(apartment08);
+        apartmentList.add(apartment09);
+        apartmentList.add(apartment10);
+        apartmentList.add(apartment11);
+        apartmentList.add(apartment12);
+        apartmentList.add(apartment13);
+        apartmentList.add(apartment14);
+        apartmentList.add(apartment15);
+        return apartmentList;
+    }
+
+
 
 }
---
+++ app/src/main/java/com/example/postventadroid/Model/Item.java
@@ -0,0 +1,31 @@
+package com.example.postventadroid.Model;
+
+public class Item {
+
+    private String itemDescription;
+    private String urlImage;
+
+    public Item(String itemDescription, String urlImage) {
+        this.itemDescription = itemDescription;
+        this.urlImage = urlImage;
+    }
+
+    public Item(String edificio_almendro, String s, String s1, String s2, String s3) {
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    public void setItemDescription(String itemDescription) {
+        this.itemDescription = itemDescription;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
+}
